480ede87fc3468d54599092b8d86dc3d6d3c0731
Simon Gruner <40799299+paleimon@users.noreply.github.com>
2024-12-07 00:33:15 +0100

    added object spawn by touching plane
    touching the plane on mobile app spawns tree object, touching screen again moves tree to new position, added assets for tree an plane

---
+++ Assets/Skripts/ARTapToPlaceObject.cs
@@ -0,0 +1,60 @@
+// using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+
+[RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
+public class ARTapToPlaceObject : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject prefab;
+    private ARRaycastManager aRRaycastManager;
+    private ARPlaneManager aRPlaneManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private GameObject spawnedObject;
+
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+        aRPlaneManager = GetComponent<ARPlaneManager>();
+
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouch.TouchSimulation.Enable();
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+        EnhancedTouch.Touch.onFingerDown += FingerDown;
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouch.TouchSimulation.Disable();
+        EnhancedTouch.EnhancedTouchSupport.Disable();
+        EnhancedTouch.Touch.onFingerDown -= FingerDown;
+    }
+
+    private void FingerDown(EnhancedTouch.Finger finger)
+    {
+        if (finger.index != 0) return;
+
+        if (aRRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(prefab, hitPose.position, hitPose.rotation);
+
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+
+        }
+    }
+}
