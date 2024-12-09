3e1a4b7d8fc3ea5acad7d616b603185e74d8907e
Kimberly Eder <kimberly.eder@t-online.de>
2024-12-09 15:21:41 +0100

    Choose Tree

--- Assets/LogoScript.cs
+++ Assets/LogoScript.cs
@@ -9,6 +9,7 @@ public class LogoScript : MonoBehaviour
     public float displayTime = 3f; // Wie lange das Logo angezeigt wird
     public GameObject arCamera; // Die AR-Kamera
     public GameObject scanArea; // Der Scan-Bereich, wie in deinem Screenshot
+    public GameObject scrollView;
 
     private void Start()
     {
@@ -16,6 +17,7 @@ public class LogoScript : MonoBehaviour
         logo.SetActive(true);
         arCamera.SetActive(false); // AR-Kamera zunächst deaktivieren
         scanArea.SetActive(false); // Den Scan-Bereich ausblenden, bis das Logo verschwunden ist
+        scrollView.SetActive(false); // Verstecke die ScrollView zu Beginn
         StartCoroutine(HideLogoAfterTime());
     }
 
@@ -25,5 +27,13 @@ public class LogoScript : MonoBehaviour
         logo.SetActive(false); // Logo ausblenden
         arCamera.SetActive(true); // AR-Kamera aktivieren, um die reale Welt zu zeigen
         scanArea.SetActive(true); // Den Scan-Bereich einblenden
+    }
+
+     // Methode, die aufgerufen wird, wenn der Scanbereich angeklickt wird
+    public void OnScanAreaClicked()
+    {
+        // Zeige die ScrollView an, wenn der Scanbereich angeklickt wurde
+        scrollView.SetActive(true);
+        scanArea.SetActive(false);
     }
 }
